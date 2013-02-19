f72f9d87715dcbc5babffae5cc3b9404d3dd10fc
Adam Brown <adam@novoda.com>
2013-02-19 19:51:46 +0000

    adding edit text and label as a better example of undoing

--- demo/src/com/ouchadam/undobar/MainActivity.java
+++ demo/src/com/ouchadam/undobar/MainActivity.java
@@ -5,49 +5,67 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements UndoBar.Callback<MainActivity.MyObject>, View.OnClickListener {
+public class MainActivity extends Activity implements UndoBar.Callback<MainActivity.TextHolder>, View.OnClickListener {
 
-    private UndoBar<MyObject> undoBar;
-    private Button button;
+    private UndoBar<TextHolder> undoBar;
+    private TextView label;
+    private EditText editText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        initViews();
+    }
+
+    private void initViews() {
+        label = (TextView) findViewById(R.id.label);
+        editText = (EditText) findViewById(R.id.edit_text);
         initUndoBar();
-        initButton();
+        initSetTextButton();
     }
 
     private void initUndoBar() {
-        ViewGroup parentView = (ViewGroup) findViewById(R.id.parent_view);
-        undoBar = new UndoBar<MyObject>(parentView, getLayoutInflater(), this);
+        ViewGroup parentView = (ViewGroup) findViewById(R.id.undobar_container);
+        undoBar = new UndoBar<TextHolder>(parentView, getLayoutInflater(), this);
     }
 
-    private void initButton() {
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
-    }
-
-    @Override
-    public void onUndo(MyObject what) {
-        Toast.makeText(this, "onUndo : " + what.getMessage(), Toast.LENGTH_SHORT).show();
+    private void initSetTextButton() {
+        Button setTextButton = (Button) findViewById(R.id.button);
+        setTextButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        undoBar.showUndoBar(false, "My undo message", new MyObject());
+        String newText = editText.getText().toString();
+        String previousText = label.getText().toString();
+        label.setText(newText);
+        undoBar.showUndoBar(false, "Undo set text?", new TextHolder(previousText));
     }
 
-    public class MyObject {
+    @Override
+    public void onUndo(TextHolder what) {
+        Toast.makeText(this, "set text to : " + what.getMessage(), Toast.LENGTH_SHORT).show();
+        label.setText(what.getMessage());
+    }
 
-        private final String message = "Some undo logic!";
+    public class TextHolder {
+
+        private final String message;
+
+        public TextHolder(String message) {
+            this.message = message;
+        }
 
         public String getMessage() {
             return message;
         }
 
     }
+
 }
 
